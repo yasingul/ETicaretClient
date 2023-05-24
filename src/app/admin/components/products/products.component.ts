2273d3299248302601cb01a7170a88bedf3e7f97
yasingul <yasngl46@gmail.com>
2023-05-24 19:46:14 +0300

    Ürün Ekleme İşlemleri Yapıldı- ProductServce

--- src/app/admin/components/products/products.component.ts
+++ src/app/admin/components/products/products.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { BaseComponent, SpinnerType } from 'src/app/base/base.component';
-import { Product } from 'src/app/contracts/product';
 import { HttpClientService } from 'src/app/services/common/http-client.service';
 
 @Component({
@@ -16,41 +15,7 @@ export class ProductsComponent extends BaseComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.showSpinner(SpinnerType.BallAtom)
-    //HttpClientService deneme kodu
     
-    this.httpClientService.get<Product[]>({
-      controller: "products"
-    }).subscribe(data => console.log(data));
-
-    // this.httpClientService.post({
-    //   controller: "products"
-    // }, {
-    //   name: "kalem",
-    //   stock: 100,
-    //   price: 15
-    // }).subscribe();
-
-    // this.httpClientService.post({
-    //   controller: "products"
-    // }, {
-    //   name: "kağıt",
-    //   stock: 1000,
-    //   price: 7
-    // }).subscribe();
-
-    // this.httpClientService.post({
-    //   controller: "products"
-    // }, {
-    //   name: "silgi",
-    //   stock: 500,
-    //   price: 4
-    // }).subscribe();
-
-    // this.httpClientService.delete({
-    //   controller: "products"
-    // }, "ef2110a6-b5a9-47ad-af9b-9f3945d52b07")
-    // .subscribe();
   }
 
 }
