4b1762531f39090491dcfa9e39549345365cb2a9
yasingul <yasngl46@gmail.com>
2023-05-26 18:41:32 +0300

    Fluent Validation İle ValidationFilter ürün ekleme kısmına eklendi.

--- src/app/admin/components/products/create/create.component.ts
+++ src/app/admin/components/products/create/create.component.ts
@@ -27,11 +27,37 @@ export class CreateComponent extends BaseComponent implements OnInit {
     create_product.stock = parseInt(stock.value);
     create_product.price = parseFloat(price.value);
 
+
+    //ui tabanlı validasyon deneme kodları
+    if(!name.value){
+      this.alertify.message("Lütfen ürün adını boş bırakmayınız.", {
+        dismissOthers:true,
+        messageType: MessageType.Error,
+        position: Position.TopCenter
+      });
+      return;
+    }
+
+    if(parseInt(stock.value) < 0){
+      this.alertify.message("Lütfen stok bilgisini geçerli bir değer olarak giriniz.", {
+        dismissOthers:true,
+        messageType: MessageType.Error,
+        position: Position.TopCenter
+      });
+      return;
+    }
+
     this.productService.create(create_product, () => {
       this.hideSpinner(SpinnerType.BallAtom);
       this.alertify.message("Ürün Başarıyla Eklendi", {
         dismissOthers:true,
         messageType: MessageType.Success,
+        position: Position.TopRight
+      });
+    }, errorMessage => {
+      this.alertify.message(errorMessage, {
+        dismissOthers:true,
+        messageType: MessageType.Error,
         position: Position.TopRight
       });
     });
